20b44e8ddc1ae34e261fa617bad29f7181194f27
mehrsasalmanipour <mehrsa.salmanipour@gmail.com>
2025-05-11 18:14:24 +0330

    Game and Game map

--- src/models/Game.java
+++ src/models/Game.java
@@ -1,5 +1,53 @@
 package models;
 
-public class Game {
+import models.enums.Weather;
 
+import java.util.List;
+
+public class Game {
+    private List<Player> players;
+    private Player currentPlayer;
+    private List<Shop> shops;
+    private int currentPlayerIndex = 0;
+    private Weather currentWeather;
+    private List<GameMap> gameMaps;
+
+    public Game(List<Shop> shops, List<Player> players, Weather startingWeather) {
+        this.shops = shops;
+        this.players = players;
+        if (!players.isEmpty()) {
+            this.currentPlayer = players.get(0);
+        }
+        this.currentWeather = startingWeather;
+    }
+
+    public List<Shop> getShops() {
+        return shops;
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public Player getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public void setCurrentPlayer(Player player) {
+        this.currentPlayer = player;
+    }
+    public Weather getCurrentWeather() {
+        return currentWeather;
+    }
+    public void setCurrentWeather(Weather weather) {
+        this.currentWeather = weather;
+    }
+
+    public void switchTurn() {
+        if (players == null || players.isEmpty()) return;
+
+        currentPlayerIndex = (currentPlayerIndex + 1) % players.size();
+        currentPlayer = players.get(currentPlayerIndex);
+        currentPlayer.resetTurnEnergy();
+    }
 }
--- src/models/GameMap.java
+++ src/models/GameMap.java
@@ -1,55 +1,32 @@
 package models;
 
+import models.enums.Weather;
+
 import java.util.List;
 
 public class GameMap {
     private Tile[][] tiles;
-    private List<Shop> shops;
-    private List<Player> players;
     int width;
     int height;
-    private Player currentPlayer;
-    private int currentPlayerIndex = 0;
 
-    public GameMap(Tile[][] tiles, int width, int height , List<Shop> shops, List<Player> players) {
+    public GameMap(Tile[][] tiles, int width, int height , List<Shop> shops, List<Player> players, Weather startingWeather) {
         this.tiles = tiles;
         this.width = width;
         this.height = height;
-        this.shops = shops;
-        this.players = players;
-        if (!players.isEmpty()) {
-            this.currentPlayer = players.get(0);
-        }
     }
     public Tile[][] getTiles() {
         return tiles;
     }
+
     public void setTiles(Tile[][] tiles) {
         this.tiles = tiles;
     }
-    public List<Shop> getShops() {
-        return shops;
-    }
-    public List<Player> getPlayers() {
-        return players;
-    }
+
     public int getWidth() {
         return width;
     }
+
     public int getHeight() {
         return height;
-    }
-    public Player getCurrentPlayer() {
-        return currentPlayer;
-    }
-    public void setCurrentPlayer(Player player) {
-        this.currentPlayer = player;
-    }
-    public void switchTurn() {
-        if (players == null || players.isEmpty()) return;
-
-        currentPlayerIndex = (currentPlayerIndex + 1) % players.size();
-        currentPlayer = players.get(currentPlayerIndex);
-        currentPlayer.resetTurnEnergy();
     }
 }
