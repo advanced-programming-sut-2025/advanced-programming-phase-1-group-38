95345409926fbfba20230671d4ae3bf7597ab492
AminShabani <amin.shabani84@yahoo.com>
2025-05-18 05:05:03 +0330

    debug gamemenu

--- src/models/enums/Commands/GameMenuCommands.java
+++ src/models/enums/Commands/GameMenuCommands.java
@@ -5,8 +5,8 @@ import java.util.regex.Pattern;
 
 public enum GameMenuCommands implements Command {
 
-    NEW_GAME("^game\\s+new\\s+-u(\\s+\\S+){1,3}$"),
-    GAME_MAP("^game\\s+map\\s+(?<mapNumber>\\d+)$"),
+    GAME_NEW("^game\\s+new\\s+-u\\s+(?<usernames>(\\w+\\s*){1,3})$"),
+    GAME_CHOOSE_MAP("^game\\s+map\\s+(?<mapNumber>\\d+)$"),
     LOAD_GAME("^load\\s+game$"),
     MENU_ENTER("^menu\\s+enter\\s+(?<menu>\\S+)$"),
     MENU_EXIT("^menu\\s+exit$"),
