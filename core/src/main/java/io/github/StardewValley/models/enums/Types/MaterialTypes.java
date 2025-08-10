32b35cdd955435bfdd214fd8558a39af1bd1cd6e
AminShabani <amin.shabani84@yahoo.com>
2025-08-11 02:39:06 +0330

    some assets

--- core/src/main/java/io/github/StardewValley/models/enums/Types/MaterialTypes.java
+++ core/src/main/java/io/github/StardewValley/models/enums/Types/MaterialTypes.java
@@ -1,8 +1,6 @@
 package io.github.StardewValley.models.enums.Types;
 
-import io.github.StardewValley.models.ItemType;
-
-public interface MaterialTypes extends ItemType {
+public interface MaterialTypes {
     boolean isTool();
     String getName();
 
