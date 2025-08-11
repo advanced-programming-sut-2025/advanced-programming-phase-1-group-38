5fde89fe1cb7eafd4ca619431900e79ccef4e9b3
mehrsasalmanipour <mehrsa.salmanipour@gmail.com>
2025-08-11 04:01:16 +0330

    added world and npc map

--- core/src/main/java/io/github/StardewValley/models/QuestBook.java
+++ core/src/main/java/io/github/StardewValley/models/QuestBook.java
@@ -10,18 +10,18 @@ public final class QuestBook {
         // ROBIN
         List<QuestDef> robin = new ArrayList<>();
         robin.add(QuestDef.b(1, "Deliver 80 Wood")
-            .req(CropType.CARROT, 80)
+            .req(MaterialType.Wood, 80)
             .gold(1000)
             .build());
 
         robin.add(QuestDef.b(2, "Deliver 10 Iron")
             .minLv(1) // requires Lv1 friendship
-            .req(CropType.CORN, 10)
+            .req(MaterialType.IronBar, 10)
             .friendPts(200) // +1 heart
             .build());
 
         robin.add(QuestDef.b(3, "Deliver 1000 Wood")
-            .req(CropType.CARROT, 1000)
+            .req(MaterialType.Wood, 1000)
             .gold(2500)
             .build());
 
